986a1679c5253e9945c082c58fb3c1e671d330b0
it-dreams <rahulraj.holi888@gmail.com>
2023-06-07 18:03:09 +0530

    updated package.json file

--- src/index.js
+++ src/index.js
@@ -3,23 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { extendTheme, ChakraProvider } from '@chakra-ui/react';
-
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-};
-
-const theme = extendTheme({ colors });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider theme={theme}>
+  <React.StrictMode>
     <App />
-  </ChakraProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
