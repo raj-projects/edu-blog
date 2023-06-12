f5578b609ba0978e745937be2d5e9bc6ce20a6a1
it-dreams <rahulraj.holi888@gmail.com>
2023-06-12 15:16:54 +0530

    Initial Commit

--- client/src/comonents/App.js
+++ client/src/comonents/App.js
@@ -10,30 +10,37 @@ import Home from '../pages/userPanel/Home';
 import Categories from '../pages/userPanel/Categories';
 import Contact from '../pages/userPanel/Contact';
 import Dashboard from '../pages/adminPanel/Dashboard';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <Container sx={{ marginTop: '80px' }}>
-        <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="about" element={<About />} />
-              <Route path="category" element={<Categories />} />
-              <Route path="contact" element={<Contact />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
+      <ApolloProvider client={client}>
+        <Header />
+        <Container sx={{ marginTop: '80px' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="category" element={<Categories />} />
+                <Route path="contact" element={<Contact />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+            </Grid>
+            <Grid item xs={4}>
+              <Sidebar />
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <Sidebar />
-          </Grid>
-        </Grid>
-      </Container>
-      <Footer />
+        </Container>
+        <Footer />
+      </ApolloProvider>
     </>
   );
 }
